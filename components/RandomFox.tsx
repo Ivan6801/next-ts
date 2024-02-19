63c435e267fdfdd82be7a8bffa5957584a583cb7
Ivan Gonzalez <hal308366@gmail.com>
2024-02-19 10:34:14 -0800

    Tipos para referencias y observadores

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
+import { useRef } from "react";
+
 type Props = {
   image: string;
 };
 
 export function RandomFox({ image }: Props): JSX.Element {
+  const node = useRef<HTMLImageElement>(null);
+
   return (
     <img
+      ref={node}
       width="320"
       height="auto"
       src={image}
